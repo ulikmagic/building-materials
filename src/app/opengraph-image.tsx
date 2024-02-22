581e740de4eaf98003aebdc516ddb34a2ad13df2
ulikmagic <ulik.magic@gmail.com>
2024-02-22 20:25:51 +0600

    fix

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -4,12 +4,12 @@ import LogoIcon from '@/assets/icons/logo.svg'
 export const runtime = 'edge'
  
 export const size = {
-  width: 200,
-  height: 200,
+  width: 100,
+  height: 100,
 }
  
-export const contentType = 'image/svg'
+export const contentType = 'image/png'
  
-const Image = () => new ImageResponse(<LogoIcon className="text-blue-dark" />, size)
+const Image = () => new ImageResponse(<div className='text-blue-dark'><LogoIcon /></div>, size)
 
 export default Image
