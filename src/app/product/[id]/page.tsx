1325515afd434f48ac7e1ac01bdb7e6211ad4840
ulikmagic <ulik.magic@gmail.com>
2024-02-23 21:46:51 +0600

    fix mistake

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Info from './components/Info'
 import Empty from './components/Empty'
 import { getProducts } from '@/utils/api'
+import { IProduct } from '@/types/api'
 
 interface ProductProps {
   params: {
@@ -14,7 +15,7 @@ const Product: NextPage<ProductProps> = async ({ params }) => {
   const products = await getProducts()
   const current = products.find((item: any) => item.id === +params.id)
 
-  return current ? <Info product={current} /> : <Empty />
+  return current ? <Info product={current as IProduct} /> : <Empty />
 }
 
 export default Product
